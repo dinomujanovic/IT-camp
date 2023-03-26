cb6529f7fd274e9f7665723cf0eee70062889032
dinomujanovic <dinomujan97@gmail.com>
2023-03-26 12:22:20 +0200

    operatori

---
+++ JavaScript/3.cas/app1.js
@@ -0,0 +1,65 @@
+//  OPERATORI U JS
+
+// postoji 6 vrsta operatora u JS
+
+// 1. aritmeticki operatori
+// 2. operatori dodele vrednosti
+// 3. string operatori
+// 4. operatori poredjenja
+// 5. logicki operatori
+// 6. tipovni operatori
+
+// 1. aritmeticki operatori:
+
+// + sabiranje
+console.log(10 + 15);
+
+// - oduzimanje
+console.log(10 - 15);
+
+// * mnozenje
+console.log(10 * 15);
+
+// ** stepenovanje
+console.log(2 ** 3);
+
+// / deljenje
+console.log(8 / 2);
+
+// % procenat
+console.log(12 % 5);
+
+// parni brojevi
+
+// neparni brojevi
+
+// ++ increment(povecanje vrednosti za 1)
+
+// -- decrement(smanjenje vrednosti za 1)
+
+// 2. operatori dodele vrednosti:
+
+// = dodeljivanje vrednosti nekoj promenljivoj
+a = 5;
+
+// += povecanje vrednosti postojece promenljive
+a += 5;
+console.log(a);
+
+// -= smanjenje vrednosti postojece promenljive
+a -= 7;
+console.log(a);
+
+// *= mnozenje vrednosti sa odredjenim brojem
+
+// /=  deljenje postojece vrednosti sa nekim brojem
+
+// %= pri deljenju trenutne vrednosti sa nekim brojem
+
+// **= stepenovanje postojece vrednosti nekim brojem
+
+// 3. string operatori:
+
+// pored osnovne namene za sabiranje brojeva + operator sluzi za spajanje stringova:
+
+// sabiranje broja i stringa nam daje string
