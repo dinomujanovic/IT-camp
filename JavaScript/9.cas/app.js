9615bbb7b8e6ef975f2d50116c280ca28b4ac34d
dinomujanovic <dinomujan97@gmail.com>
2023-04-24 21:28:37 +0200

    while petlja

--- JavaScript/9.cas/app.js
+++ JavaScript/9.cas/app.js
@@ -18,10 +18,15 @@
 
 // traziti od korisnika da unese neki broj, na osnovu te vrednosti da se izvrsi ispis kvadrata brojeva od 1  do tog unetog broja
 
-const broj = +prompt("unesite neki broj")
-let i = 1
-while ( i <= broj){
-    console.log( i ** 2)
-    i++
-}
+// const broj = +prompt("unesite neki broj")
+// let i = 1
+// while ( i <= broj){
+//     console.log( i ** 2)
+//     i++
+// }
 
+let vrednost = 1 
+while (vrednost !== 0) {
+    vrednost = +prompt("unesite neku vrednost")
+
+}
